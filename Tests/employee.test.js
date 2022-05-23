d0f40972388d818a75116ea508e781313788af73
George Kaluwin <georgekaluwin@gmail.com>
2022-05-23 15:25:43 +0930

    npm init and creating constructors

---
+++ Tests/employee.test.js
@@ -0,0 +1,7 @@
+const Employee = require('./lib/Employee');
+const employee = new Employee ('GEorge', '123456', 'georgekaluwin@gmail.com');
+
+test ('created a new employee.', () => {
+    const employeeInstance = new Employee();
+    expect()
+})
